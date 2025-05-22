a00935bd5a5dda0ff1477a45a1fd24f2277c0f9e
Sasha <sp.kleincharland@gmail.com>
2025-05-22 19:28:28 -0400

    add running time measurement

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from  scrapes import *
-import requests
-import random
+import time
 
 def main():
     # Call some of the scraping methods here
 
+    start = time.time()
+
     transfermarkt_mls_transfers()
+
+    print(f"----- Program Execution Time: {time.time() - start} -----")
 
 if __name__ == "__main__":
     main()
